1e95f721ab35521d349ab6131bb9263b0e9e4692
haha523 <77500794+haha523@users.noreply.github.com>
2024-09-12 09:53:05 +0300

    Add files via upload

---
+++ Task 2/try for task 2.py
@@ -0,0 +1,10 @@
+with open("d:/Visual studio code/input.txt", "r") as input_file:
+    content = input_file.read().split()
+    if len(content) < 2:
+        raise ValueError("Input file must contain at least two integers.")
+    a, b = map(int, content)
+
+sum = a + b**2
+
+with open("d:/Visual studio code/output.txt", "w") as output_file:
+    output_file.write(str(sum))
